a8cbb23614871552f55c19570f5a9c49a0606db6
shcweb <sunhaochen17505@gmail.com>
2020-03-12 17:39:14 +0800

    下位机EEPROM,串口操作

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -3,13 +3,34 @@
  Created:	2020/3/10 14:49:50
  Author:	sunha
 */
-
+#include <EEPROM.h>
+struct Keys
+{
+    byte key = B0000;
+    byte strLength = 0;
+    char str[233] = "";
+} keys;
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+    Serial.begin(115200);
+    EEPROM.get(0, keys);
+    Serial.println(keys.key);
+    Serial.println(keys.str);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
+    if (Serial.available > 0)
+    {
+        String key = Serial.readStringUntil('|');
+        keys.key = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            key += key[i] << i;
+        }
+        strcpy(keys.str, Serial.readStringUntil('>').c_str());
+        keys.strLength = strlen(keys.str);
+        EEPROM.put(0, keys);
+    }
 
 }
